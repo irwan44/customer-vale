99cf144ac4c1e062a996a672992e4635b6ed7a21
irwan44 <irwanstwn2398@gmail.com>
2024-07-06 10:19:57 +0700

    pusssss

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:pusher_channels_flutter/pusher_channels_flutter.dart';
 import 'app/componen/color.dart';
 import 'app/data/endpoint.dart';
 import 'app/data/publik.dart';
 import 'app/routes/app_pages.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('Handling a background message: ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await GetStorage.init('token-mekanik');
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
@@ -37,9 +47,13 @@ void main() async {
   runApp(const MyApp());
 }
 
-// Future<void> startPollingNotificationsApprove() async {
-//   await API.showBookingNotificationsApprove();
-// }
+void startPollingNotifications() {
+  const pollingInterval = Duration(minutes: 1);
+
+  Timer.periodic(pollingInterval, (timer) async {
+    print("Polling notification...");
+  });
+}
 
 
 
@@ -52,10 +66,115 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String? _token;
+  late PusherChannelsFlutter pusher;
+  final String appId = "1827229";
+  final String key = "5c463cc9a2fdf08932b5";
+  final String secret = "521202e4d68dc816c054";
+  final String cluster = "ap1";
+
+  @override
+  void initState() {
+    super.initState();
+    initPusher();
+    _getToken();
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+  }
+
+  Future<void> initPusher() async {
+    pusher = PusherChannelsFlutter.getInstance();
+    try {
+      await pusher.init(
+        apiKey: key,
+        cluster: cluster,
+        onEvent: (event) {
+          print("Event received: $event");
+          showLocalNotification(event.eventName, event.data);
+        },
+        onConnectionStateChange: (currentState, previousState) {
+          print("Connection state changed from $previousState to $currentState");
+        },
+        onError: (message, code, exception) {
+          print("Connection error: $message");
+        },
+      );
+      await pusher.connect();
+      await pusher.subscribe(
+        channelName: 'my-channel',
+      );
+    } catch (e) {
+      print("Pusher connection error: $e");
+    }
+  }
+
+  void _getToken() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      String? token = await messaging.getToken();
+      setState(() {
+        _token = token;
+      });
+      print("FCM Token: $_token");
+
+      // Subscribe to topic
+      messaging.subscribeToTopic('ValeBooking').then((_) {
+        print('Subscribed to allUsers topic');
+      }).catchError((error) {
+        print('Failed to subscribe to allUsers topic: $error');
+      });
+    } else {
+      print("User declined or has not accepted permission");
+    }
+  }
+
+  void showLocalNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+      'high_importance_channel', // channel_id
+      'High Importance Notifications', // channel_name
+      channelDescription: 'This channel is used for important notifications.', // channel_description
+      importance: Importance.max,
+      priority: Priority.high,
+      ticker: 'ticker',
+      showWhen: false,
+      sound: RawResourceAndroidNotificationSound('sounds'),
+    );
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'Notikasi Masuk',
+      'Status Kendaraan anada : Booking',
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import device_info_plus
 import file_selector_macos
+import firebase_core
+import firebase_messaging
 import flutter_inappwebview_macos
 import flutter_local_notifications
 import geolocator_apple
@@ -21,6 +23,8 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
